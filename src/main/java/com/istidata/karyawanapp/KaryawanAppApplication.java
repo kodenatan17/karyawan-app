79bef972cff8ac0e71c4a86b631a48ae04cdec74
kodenatan17 <kodenatan17@gmail.com>
2022-10-27 19:54:56 +0700

    first-commit

---
+++ src/main/java/com/istidata/karyawanapp/KaryawanAppApplication.java
@@ -0,0 +1,13 @@
+package com.istidata.karyawanapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaryawanAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaryawanAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/istidata/karyawanapp/KaryawanAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.istidata.karyawanapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaryawanAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
